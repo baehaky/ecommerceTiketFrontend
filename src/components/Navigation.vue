9e4d64804e984d973f044502d10a7c35872775fb
baehaky <baehaky1504@gmail.com>
2024-01-02 15:20:38 +0700

    modified:   index.html 	modified:   src/assets/main.css 	modified:   src/components/Navigation.vue 	new file:   src/components/PageNotFound.vue 	modified:   src/main.js 	modified:   src/router/index.js 	new file:   src/views/PesanView.vue

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,10 +1,35 @@
 <script setup>
+    import { ref } from 'vue';
     import { RouterLink, RouterView } from 'vue-router';
+    let open = ref(true)
+    function MenuOpen(){
+        open.value = !open.value
+    }
+    let Links = [
+        { name: "Home", path: "/" },
+        { name: "Service", path: "service" },
+        { name: "About", path: "about" },
+        { name: "Login", path: "login" },
+    ]
 </script>
 <template>
-    <nav class="bg-purple-700 p-5 space-x-5">
-        <RouterLink class="text-white" to="/">Home</RouterLink>
-        <RouterLink class="text-white" to="/about">About</RouterLink>
+    <nav class="bg-indigo-800 text-white py-3.5 px-6 shadow-slate-700/45 shadow-md md:flex justify-between items-center">
+        <div class="flex items-center">
+            <h1 class="font-bold uppercase underline underline-offset-4 md:text-2xl text-xl">SonicTickets</h1>
+        </div>
+        <span @click="MenuOpen()" class="absolute md:hidden right-6 top-1.5 cursor-pointer text-4xl">
+            <i :class="[open ?'bi bi-x' : 'bi bi-list']"></i>
+        </span>
+
+
+        <ul class="md:flex md:items-center md:static absolute md:pb-0 px-3 md:px-0 pb-10 md:w-auto w-full top-14 duration-700 bg-indigo-900 md:bg-inherit" :class="[open ? 'left-0' : 'left-[-100%]']">
+            <li class="md:mx-4 md:my-0 my-5" v-for="link in Links">
+                <RouterLink :to="link.path">{{ link.name }}</RouterLink>
+            </li>
+            <button class="md:mx-4 bg-white  p-1 rounded-md px-3 hover:bg-slate-300">
+                <RouterLink to="register" class="text-black hover:text-neutral-700">Register</RouterLink>
+            </button>
+        </ul>
     </nav>
     <RouterView/>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,42 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import AboutViewVue from '../views/AboutView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import PesanViewVue from "../views/PesanView.vue";
+import PageNotFoundVue from "@/components/PageNotFound.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/about",
+      name: "about",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      component: () => import("../views/AboutView.vue"),
+    },
+    {
+      path: "/pesan",
+      name: "pesan",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: PesanViewVue,
+    },
+    {
+      path: "/:pathMatch(.*)*",
+      name: "pesan",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: PageNotFoundVue,
+    },
   ],
-  linkActiveClass: 'text-gray-300 underline'
-})
+  linkActiveClass: "underline",
+});
 
-export default router
+export default router;
