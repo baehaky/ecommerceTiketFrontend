f36d7e53331099b91fb89cb18f592da3b2b30cf9
baehaky <baehaky1504@gmail.com>
2024-01-03 11:57:11 +0700

    modified:   src/components/Navigation.vue

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -6,10 +6,10 @@
         open.value = !open.value
     }
     let Links = [
-        { name: "Home", path: "/" },
-        { name: "Service", path: "service" },
-        { name: "About", path: "about" },
-        { name: "Login", path: "login" },
+        { id: 1, name: "Home", path: "/" },
+        { id: 2, name: "Service", path: "service" },
+        { id: 3, name: "About", path: "about" },
+        { id: 4, name: "Login", path: "login" },
     ]
 </script>
 <template>
@@ -23,8 +23,8 @@
 
 
         <ul class="md:flex md:items-center md:static absolute md:pb-0 px-3 md:px-0 pb-10 md:w-auto w-full top-14 duration-700 bg-indigo-900 md:bg-inherit" :class="[open ? 'left-0' : 'left-[-100%]']">
-            <li class="md:mx-4 md:my-0 my-5" v-for="link in Links">
-                <RouterLink :to="link.path">{{ link.name }}</RouterLink>
+            <li class="md:mx-4 md:my-0 my-5" v-for="link in Links" :key="link.id">
+                <RouterLink :to="link.path"> {{ link.name }}</RouterLink>
             </li>
             <button class="md:mx-4 bg-white  p-1 rounded-md px-3 hover:bg-slate-300">
                 <RouterLink to="register" class="text-black hover:text-neutral-700">Register</RouterLink>
